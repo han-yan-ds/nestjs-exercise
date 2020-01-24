3250220e95b16058d2d2d10db2000d9397f7c4a3
han-yan-ds <hanprogramming@gmail.com>
2020-01-24 13:53:40 -0700

    Added TaskModel... don't confuse this with model from MVC (that's Service)

---
+++ src/tasks/tasks.model.ts
@@ -0,0 +1,15 @@
+/**
+ * Creating a Task Model (right now an interface, could be class later)
+ */
+export enum TaskStatus { 
+  Pending = 'pending', 
+  Active = 'active', 
+  Finished = 'finished'
+};
+
+export interface Task {
+  id: number,
+  title: string,
+  description: string,
+  status: TaskStatus,
+}
