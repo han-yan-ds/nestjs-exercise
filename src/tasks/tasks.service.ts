f57bc28cea6ab64b4a70b956a0c1ebce8ea0d0a0
han-yan-ds <hanprogramming@gmail.com>
2020-01-24 14:08:24 -0700

    Created a createTask method in Service

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -3,13 +3,26 @@
  */
 
 import { Injectable, Get } from '@nestjs/common';
-import { Task } from './tasks.model';
+import { Task, TaskStatus } from './tasks.model';
 
 @Injectable()
 export class TasksService {
   private tasksArr: Task[] = []; // temporary, will eventually be in database instead
+  private id = 0;
 
   getAllTasks(): Task[] {
     return this.tasksArr;
+  }
+
+  createTask(title: string, description: string, numPeople: number): void {
+    const task: Task = {
+      id: this.id,
+      title,
+      description,
+      numPeople,
+      status: TaskStatus.Pending
+    }
+    this.tasksArr.push(task);
+    this.id++;
   }
 }
