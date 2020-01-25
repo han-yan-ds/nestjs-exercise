258d72cf28286813534fb851de915be8c26276fd
han-yan-ds <hanprogramming@gmail.com>
2020-01-25 12:02:56 -0700

    Added MinLength validation to title and description, AND changed DTOs to classes (from interfaces)

--- src/tasks/tasks.model.ts
+++ src/tasks/tasks.model.ts
@@ -1,21 +1,25 @@
 /**
  * Creating a Task Model (right now an interface, could be class later)
  */
+import { MinLength } from 'class-validator';
+
 export enum TaskStatus { 
   Pending = 'Pending', 
   Active = 'Active', 
   Finished = 'Finished'
 };
 
-export interface CreateTaskDto {
+export class CreateTaskDto { 
+  /* DTOs need to be classes instead of interfaces in order 
+   * for class-validator decorators to work! */
   /* Purpose of this is to make sure the body of the createTask request has
    * title, description, and numPeople */
-  title: string,
-  description: string,
-  numPeople: number,
+  @MinLength(2) title: string
+  @MinLength(5) description: string
+  numPeople: number
 }
 
-export interface Task extends CreateTaskDto {
-  id: string,
-  status: TaskStatus,
+export class Task extends CreateTaskDto {
+  id: string
+  status: TaskStatus
 }
